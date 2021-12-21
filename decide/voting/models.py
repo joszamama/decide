98244b791bb6fbd75a7372c86ea9ec85bcc7f90a
rodsangon <rodsangon@alum.us.es>
2021-12-21 19:31:47 +0100

    Revert "feat(voting): Añadido campo preference para PreferenceQuestions"
    This reverts commit 422388d04c90771d7eb2a61a1521f3973e6ac2eb.

--- decide/voting/models.py
+++ decide/voting/models.py
@@ -32,7 +32,6 @@ class Voting(models.Model):
     name = models.CharField(max_length=200)
     desc = models.TextField(blank=True, null=True)
     question = models.ForeignKey(Question, related_name='voting', on_delete=models.CASCADE)
-    preferencia = models.BooleanField(default=False)
 
     start_date = models.DateTimeField(blank=True, null=True)
     end_date = models.DateTimeField(blank=True, null=True)
