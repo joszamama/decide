668bcce2088242b1c2f8a539cd8acb2ccf749e60
pabmatale <pmateosalex@hotmail.com>
2022-01-08 09:52:39 +0100

    test(LoginSelenium):Implementados tests LoginWithoutRegister & LoginWithNoFields
    Se han implementado dos nuevos test:
    -testLoginWithoutRegister comprueba que al introducir un usuario no registrado te mantiene en la página /account/login.
    -testLoginWithNoFields comprueba que al no introducir ningun campo de inicio de sesion la web se mantiene en /account/loggin para iniciar sesion correctamente
    Ambos realizados en el archivo test.py del modulo account

--- decide/account/test.py
+++ decide/account/test.py
@@ -3,46 +3,80 @@ from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from django.contrib.auth.models import User
+from base.tests import BaseTestCase
 
 class LoginTestSelenium(LiveServerTestCase):
 
-  def createUserInDB(self):
-        user = User()
-        user.username = 'Prueba2k22'
-        user.email = 'prueba2k22@gmail.com'
-        user.set_password('prueba')
-        user.save()
+      def createUserInDB(self):
+            user = User()
+            user.username = 'Prueba2k22'
+            user.email = 'prueba2k22@gmail.com'
+            user.set_password('prueba')
+            user.save()
 
 
-  def testLogin(self):
-        selenium = webdriver.Chrome()
-        selenium.get('http://localhost:8000/account/login/')
+      def testLogin(self):
+            selenium = webdriver.Chrome()
+            selenium.get('http://localhost:8000/account/login/')
 
-        time.sleep(5)
-    
-        userEmail = selenium.find_element_by_name('email')
-        userPass = selenium.find_element_by_name('password')
+            time.sleep(5)
+      
+            userEmail = selenium.find_element_by_name('email')
+            userPass = selenium.find_element_by_name('password')
 
-        userEmail.send_keys('prueba2k22@gmail.com')
-        userPass.send_keys('1234',Keys.ENTER)
+            userEmail.send_keys('prueba2k22@gmail.com')
+            userPass.send_keys('1234',Keys.ENTER)
 
-        time.sleep(5)
+            time.sleep(5)
 
-        assert 'prueba2k22@gmail.com' in selenium.page_source
+            assert 'prueba2k22@gmail.com' in selenium.page_source
 
    
-  def testLoginWithWrongPassword(self):
-        selenium = webdriver.Chrome()
-        selenium.get('http://localhost:8000/account/login/')
+      def testLoginWithWrongPassword(self):
+            selenium = webdriver.Chrome()
+            selenium.get('http://localhost:8000/account/login/')
 
-        time.sleep(5)
+            time.sleep(5)
 
-        userEmail = selenium.find_element_by_name('email')
-        userPass = selenium.find_element_by_name('password')
+            userEmail = selenium.find_element_by_name('email')
+            userPass = selenium.find_element_by_name('password')
 
-        userEmail.send_keys('prueba2k22@gmail.com')
-        userPass.send_keys('contraseñaErronea',Keys.ENTER)
+            userEmail.send_keys('prueba2k22@gmail.com')
+            userPass.send_keys('contraseñaErronea',Keys.ENTER)
 
-        time.sleep(5)
+            time.sleep(5)
 
-        assert 'Usuario o contraseña incorrecta' in selenium.page_source
+            assert 'Usuario o contraseña incorrecta' in selenium.page_source
+
+  
+      def testLoginWithoutRegister(self):
+            selenium = webdriver.Chrome()
+            selenium.get('http://localhost:8000/account/login/')
+
+            time.sleep(5)
+      
+            userEmail = selenium.find_element_by_name('email')
+            userPass = selenium.find_element_by_name('password')
+
+            userEmail.send_keys('usuarioNoRegistrado@gmail.com')
+            userPass.send_keys('1234',Keys.ENTER)
+
+            time.sleep(5)
+
+            assert 'account/login' in selenium.current_url
+
+      def testLoginWithNoFields(self):
+            selenium = webdriver.Chrome()
+            selenium.get('http://localhost:8000/account/login/')
+
+            time.sleep(5)
+      
+            userEmail = selenium.find_element_by_name('email')
+            userPass = selenium.find_element_by_name('password')
+
+            userEmail.send_keys('')
+            userPass.send_keys('',Keys.ENTER)
+
+            time.sleep(5)
+
+            assert 'account/login' in selenium.current_url
