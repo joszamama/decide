dd4d318b5a8286830cd1867a2c23f559bf573f15
pabmatale <pmateosalex@hotmail.com>
2022-01-08 11:50:19 +0100

    test(Login Selenium Redes Sociales): Creacion de Test Twitter
    -Creacion del test de Twitter en test.py asi como sus variables de uso

--- decide/account/test.py
+++ decide/account/test.py
@@ -9,8 +9,11 @@ BASEURL='https://localhost:8000/account/login/'
 
 PROFILE_WEB = 'https://localhost:8000/account/profile/'
 
-USER_TWITCH = 'ProjectTrys' #Introducir usuario de Twitch para las pruebas
-PASSWORD_TWITCH = 'Cpa3q7MtgyHJhCn' #Introducir contraseña de Twitch para las pruebas
+USER_TWITCH = '' #Introducir usuario de Twitch para las pruebas
+PASSWORD_TWITCH = '' #Introducir contraseña de Twitch para las pruebas
+
+USER_TWITTER = '' #Introducir usuario de Twitter para las pruebas
+PASSWORD_TWITTER = '' #Introducir contraseña de Twitter para las pruebas
 
 class LoginRSTestSelenium(LiveServerTestCase):                  #Estos tests deben realizarse usando https.
   
@@ -58,3 +61,28 @@ class LoginRSTestSelenium(LiveServerTestCase):                  #Estos tests deb
         time.sleep(3)  
 
         assert ('Introduce el código que encontrarás en la aplicación de autenticación o solicítalo mediante un SMS.' in selenium.page_source) and ('https://www.twitch.tv/login' in selenium.current_url)
+
+    def testAccederTwitter(self):
+        selenium = webdriver.Chrome()
+        selenium.get(BASEURL)
+            
+        acceptButton=selenium.find_element_by_id('details-button')
+        acceptButton.click()
+        time.sleep(2)
+            
+        goButton=selenium.find_element_by_id('proceed-link')
+        goButton.click()
+        time.sleep(3)
+
+        twitterButton = selenium.find_element_by_name('twitterButton')
+        twitterButton.click()
+        time.sleep(3)  
+
+        username = selenium.find_element_by_id('username_or_email')
+        userPass = selenium.find_element_by_id('password')
+
+        username.send_keys(USER_TWITTER)
+        userPass.send_keys(PASSWORD_TWITTER,Keys.ENTER)
+        time.sleep(3)
+
+        assert (PROFILE_WEB in selenium.current_url) and (USER_TWITTER in selenium.page_source)
