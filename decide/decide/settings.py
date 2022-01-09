8a49c66fec7c45d61339855e53c714c305447312
gonrodter <gonrodter@alum.us.es>
2022-01-09 17:55:20 +0100

    fix(Heroku): Heroku deployment fix v2

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -10,7 +10,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 import django_heroku
 
-django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -228,3 +227,5 @@ INSTALLED_APPS = INSTALLED_APPS + MODULES
 APIS = {}
 
 BASEURL = 'https://decide-mulhacen-3.herokuapp.com'
+
+django_heroku.settings(locals())
