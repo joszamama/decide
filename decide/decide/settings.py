40fad0b6c95f3a8754a383caef4917bc7b0bebee
gonrodter <gonrodter@alum.us.es>
2022-01-09 17:19:49 +0100

    Heroku deployment

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku
+
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -180,3 +183,7 @@ if os.path.exists("config.jsonnet"):
 
 
 INSTALLED_APPS = INSTALLED_APPS + MODULES
+
+APIS = {}
+
+BASEURL = 'https://decide-mulhacen-3.herokuapp.com'
